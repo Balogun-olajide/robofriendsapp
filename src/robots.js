27d68433e099d6dfd98e5ae52c6a9925f23525b8
Balogun-olajide <olajidebalogun@icloud.com>
2023-03-16 02:27:47 +0100

    First commit

---
+++ src/robots.js
@@ -0,0 +1,69 @@
+export const robots = [
+{
+    id: 0, 
+    name: 'Bob builder',
+    username: 'Bob',
+    email: 'bobbuilder.biz'
+},
+{
+    id: 1,
+    name: 'james cloud',
+    username: 'jamez',
+    email: 'jamesclou@gmail.com'
+},
+{
+    id: 2,
+    name: 'Mr Vakel',
+    username: 'Mr.V',
+    email: 'Mrvakel@icloud.com'
+},
+{
+    id: 3,
+    name: 'Miss Eliz',
+    username: 'Eliz',
+    email: 'Eliz@icloud.com'
+},
+{
+    id: 4,
+    name: 'Jon',
+    username: 'jonathan',
+    email: 'Jonathan@yahoo.com'
+},
+{
+    id: 5,
+    name: 'Kunle',
+    username: 'Adekunle',
+    email: 'Adekunle1@yahoo.com'
+},
+{
+    id: 6,
+    name: 'simi',
+    username: 'simisola',
+    email: 'simisolawow@yahoo.com'
+},
+{
+    id: 7,
+    name: 'Kiki',
+    username: 'Kiki_1',
+    email: 'Kikicares@icloud.com'
+},
+{
+    id: 8,
+    name: 'promz',
+    username: 'promz*',
+    email: 'promz12@yahoo.com'
+},
+{
+    id: 9,
+    name: 'peak',
+    username: 'peak_charles',
+    email: 'peakcharles.biz'
+},
+{
+    id: 10,
+    name: 'David',
+    username: 'David2',
+    email: 'JDavid@gmail.com'
+},
+
+]
