a11a71ac69d052bf5b4fe2c3c74d306f811c941f
Balogun-olajide <olajidebalogun@icloud.com>
2023-05-20 15:46:12 +0100

    Added thunkMiddleware

--- src/index.js
+++ src/index.js
@@ -3,13 +3,14 @@ import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux'; // Import unstable_createStore for concurrent mode
 import { createLogger } from 'redux-logger';
+import thunkMiddleware from 'redux-thunk';
 import './index.css';
 import 'tachyons';
 import App from './containers/App';
 import { searchRobots } from './reducers';
 
 const logger = createLogger();
-const store = createStore(searchRobots, applyMiddleware(logger));
+const store = createStore(searchRobots, applyMiddleware(thunkMiddleware, logger));
 
 const root = ReactDOM.createRoot(document.getElementById('root')); // Create concurrent mode root
 root.render(
